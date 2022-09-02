c404fa5b75bbbfa2fc8dd767b1a72c8d4356ddbf
Lord-james505 <adeagbojames3991@gmail.com>
2022-09-02 12:31:02 +0000

    correction

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const strings = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (const string of strings) {
-  console.log(string);
+for (let i = 0; i < 3; i++) {
+  console.log(strings[i]);
 }
